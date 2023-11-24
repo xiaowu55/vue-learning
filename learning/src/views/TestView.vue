8bf36ef954d53c73b80dac56d521eeedb78561c7
jiazewu <jiazewu001@gmail.com>
2023-11-24 19:31:29 +0800

    feat:test实现

---
+++ learning/src/views/TestView.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue';
+
+const count = ref(0)
+
+function increment() {
+    count.value++
+}
+
+onMounted(() => {
+    console.log(`The initial count is ${count.value}`)
+})
+</script>
+
+<template>
+    <div>
+        <button @click="increment">Count is :{{ count }}</button>
+    </div>
+</template>
